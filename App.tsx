254b10f8a1c4442fe883cc047df582de1e240a5a
Rômulo Valentim <valentim.canejo@hotmail.com>
2025-06-08 15:53:48 -0300

    feat: install nativewind and gluestack

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import './global.css';
+import { GluestackUIProvider } from './src/components/ui/gluestack-ui-provider';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <GluestackUIProvider>
+      <AppNavigator />
+    </GluestackUIProvider>
+  );
 }
